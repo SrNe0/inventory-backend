56a7b005617fbbd20b2814c400187edc1d91e001
SrNe0 <diegosistemas29@gmail.com>
2022-08-11 16:03:44 -0500

    servicio guardar categoria

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
 
@@ -30,6 +33,7 @@ public class CategoryRestController {
 	}
 	/**
 	 * Get all the categories by id
+	 * @param id
 	 * @return
 	 */
 	@GetMapping("/categories/{id}")
@@ -39,5 +43,16 @@ public class CategoryRestController {
 		return response;
 		
 	}
-	
+	/**
+	 * save categories
+	 * @param Category
+	 * @return
+	 */
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> saveCategoriesById(@RequestBody Category category){
+		
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+		
+	}
 }
