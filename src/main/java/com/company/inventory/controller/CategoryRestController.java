97b7fea6e0dfd97d1495f680c013493e690f9a65
SrNe0 <diegosistemas29@gmail.com>
2022-09-04 17:37:20 -0500

    Servicio Actualizar

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,9 +50,23 @@ public class CategoryRestController {
 	 * @return
 	 */
 	@PostMapping("/categories")
-	public ResponseEntity<CategoryResponseRest> saveCategoriesById(@RequestBody Category category){
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
 		
 		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+		
+	}
+	
+	/**
+	 * update categories
+	 * @param category
+	 * @param id
+	 * @return
+	 */
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category,@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = service.update(category,id);
 		return response;
 		
 	}
--- src/main/java/com/company/inventory/services/CategoryServicesImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServicesImpl.java
@@ -109,7 +109,53 @@ public class CategoryServicesImpl implements ICategoryService{
 	}
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
-}
+
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> update(Category category, Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			
+			Optional<Category> categorySearch = categoryDao.findById(id);
+			
+			if (categorySearch.isPresent()) {
+				//Se procedera a actualizar el registro
+				categorySearch.get().setName(category.getName());
+				categorySearch.get().setDescription(category.getDescription());
+				
+				Category categoryToUpdate = categoryDao.save(categorySearch.get());
+				
+				if(categoryToUpdate != null) {
+					list.add(categoryToUpdate);
+					response.getCategoryResponse().setCategory(list);
+					response.setMetadata("Respuesta ok", "00", "Categoria Actualizada");
+				}else {
+					response.setMetadata("Respuesta nok", "-1", "Categoria no Actualizada");
+					return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+				}
+				
+			}else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+				
+			}
+			
+			
+			
+		} catch (Exception e) {
+			
+			response.setMetadata("Respuesta nok", "-1", "Error al actualizar categoria");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+	}
+
 
 
 
